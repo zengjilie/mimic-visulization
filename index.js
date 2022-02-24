901c327b0f245a5541387bca8a3e12a1c62ad85b
zengjilie <zengjilie@gmail.com>
2022-02-23 18:01:40 -0600

    added pages

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const app = express();
 //mw
 app.use(express.json());
 
+//routes
+app.get('/', (req, res) => {
+    res.sendFile('./view/index.html',{root:__dirname});
+})
 
 //port
-app.listen(5000, () =>{
+app.listen(5000, () => {
     console.log("server running on port 5000");
-    
+
 })
 
